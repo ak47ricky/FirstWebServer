b7a13733c41d15744c2f93cf2729114c57fe6492
Ricky <ak47ricky24@gmail.com>
2018-01-31 15:42:03 +0800

    留言

---
+++ AddCustomerMessage.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+public partial class AddCustomerMessage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            string vName = Request.QueryString["Name"].ToString();
+            string vEmail = Request.QueryString["Email"].ToString();
+            string vContent = Request.QueryString["Content"].ToString();
+
+            try
+            {
+                string vStr = "INSERT INTO CustomerMessage(Name,Email,Content) VALUES('" + vName +
+                                "','" + vEmail + "','" + vContent + " ')";
+
+                using (SqlConnection vCon = new SqlConnection("Data Source=184.168.47.10;Integrated Security=False;User ID=MobileDaddy;PASSWORD=Aa54380438!;Connect Timeout=15;Encrypt=False;Packet Size=4096"))
+                {
+                    vCon.Open();
+                    using (SqlCommand vCmd = new SqlCommand(vStr, vCon))
+                    {
+                        vCmd.ExecuteNonQuery();
+                    }
+                }
+                Response.Write("0");
+            }
+            catch
+            {
+                Response.Write("1");
+            }
+
+        }
+        catch
+        {
+            Response.Write("99");
+        }
+    }
+}
